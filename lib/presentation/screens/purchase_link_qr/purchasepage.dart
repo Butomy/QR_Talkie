a840f04e9d64db3514019c52cf6b26dc5a52fef6
Subi <subibabu4613@gmail.com>
2023-10-06 11:46:15 +0530

    purchase page

---
+++ lib/presentation/screens/purchase_link_qr/purchasepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Purchasepage extends StatefulWidget {
+  const Purchasepage({super.key});
+
+  @override
+  State<Purchasepage> createState() => _PurchasepageState();
+}
+
+class _PurchasepageState extends State<Purchasepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("data"),
+    );
+  }
+}
